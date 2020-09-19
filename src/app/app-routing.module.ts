3619c4aaa8db4fc18b2777861412222cf647a9f8
Bryce J. Fisher <bryce.fisher@gmail.com>
2020-09-19 17:32:26 -0600

    Add demo route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'demo',
+    loadChildren: () => import('./demo/demo.module').then((m) => m.DemoModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
