5685de7f26fdc8b0427742840b339461a873d4c7
Bryce J. Fisher <bryce.fisher@gmail.com>
2021-03-12 13:03:53 -0700

    Deploy with PROD details.

--- src/environments/general.environment.ts
+++ src/environments/general.environment.ts
@@ -1,18 +1,17 @@
 export const generalEnvironment = {
   production: false,
   // cSpell: disable
-  webSocketUrl:
-    'wss://2m22wxzt0k.execute-api.us-east-1.amazonaws.com/production',
+  webSocketUrl: 'wss://bg5jxnp6g2.execute-api.us-east-1.amazonaws.com/api/',
   // cSpell: enable
   cognitoSettings: {
     aws_project_region: 'us-east-1',
     aws_cognito_region: 'us-east-1',
     // cSpell: disable
-    aws_user_pools_id: 'us-east-1_XpUg7805q',
-    aws_user_pools_web_client_id: '6pftb927d20umi65al3vlpjmoa',
+    aws_user_pools_id: 'us-east-1_qe0Tpp6YR',
+    aws_user_pools_web_client_id: 'hrm7bd4h7ssfohncicnbsdm54',
     // cSpell: enable
     oauth: {
-      domain: 'fantasygc.auth.us-east-1.amazoncognito.com',
+      domain: 'login.fantasygc.org',
       scope: [
         'phone',
         'email',
