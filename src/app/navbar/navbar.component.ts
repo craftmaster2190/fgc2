1a4d280d30457ad08ff3b7174c72df2d9f9a9d52
Bryce J. Fisher <bryce.fisher@gmail.com>
2021-03-13 15:05:41 -0700

    Fix spinner when not logged in.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ServerBusService } from '../websocket/server-bus.service';
+import { UserHolderService } from '../websocket/user-holder.service';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { tap } from 'rxjs/operators';
 
+@UntilDestroy()
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  public constructor(public readonly serverBus: ServerBusService) {}
+  private loggedIn = false;
+
+  public constructor(
+    private readonly userHolder: UserHolderService,
+    private readonly serverBus: ServerBusService
+  ) {
+    this.userHolder
+      .watchUser()
+      .pipe(
+        untilDestroyed(this),
+        tap((value) => (this.loggedIn = !!value))
+      )
+      .subscribe();
+  }
 
   public ngOnInit(): void {}
+
+  public showSpinner(): boolean {
+    return this.loggedIn && !this.serverBus.isConnected();
+  }
 }
